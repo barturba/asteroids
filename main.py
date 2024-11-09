f3e204ee9c0c6fc9903a3980bcf20322a1401ac6
barturba <b@bartas.co>
2024-11-09 13:07:06 -0700

    Added FPS limiting

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Initialize FPS clock
+    clock = pygame.time.Clock()
+    dt = 0  # Delta time
+
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000  # Run at a max FPS of 60
 
 
 if __name__ == "__main__":
