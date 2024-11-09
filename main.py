1a1967158385b2ac34f036c7c1858c60d96a6279
barturba <b@bartas.co>
2024-11-09 11:30:26 -0700

    Draw a black background to the screen

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from constants import *
 
 import pygame
 
-pygame.init()
 
-pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def main():
+    pygame.init()
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
