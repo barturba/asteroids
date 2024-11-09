df9652cececf74fb2e7d3577ababe43198494b40
barturba <b@bartas.co>
2024-11-09 13:25:33 -0700

    Added player rotation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import *
 
 import pygame
 
@@ -12,8 +13,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0  # Delta time
 
+    # Initialize a Player object
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
