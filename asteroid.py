279d65bd6ae85de9fbc105d1cbabfd2432eef7ea
barturba <b@bartas.co>
2024-11-09 15:58:52 -0700

    Added asteroid splitting

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
 from circleshape import *
-from constants import Color
+from constants import ASTEROID_MIN_RADIUS, Color
+import random
 
 
 class Asteroid(CircleShape):
@@ -11,3 +12,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        # Create two smaller asteroids
+        random_angle = random.uniform(20.0, 50.0)
+
+        angle1 = self.velocity.rotate(random_angle)
+        angle2 = self.velocity.rotate(-random_angle)
+
+        radius1 = self.radius - ASTEROID_MIN_RADIUS
+        radius2 = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, radius1)
+        asteroid2 = Asteroid(self.position.x, self.position.y, radius2)
+
+        asteroid1.velocity = angle1 * 1.2
+        asteroid2.velocity = angle2
--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
         for asteroid in asteroids:
             for bullet in shots:
                 if bullet.collided(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     bullet.kill()
 
         # Run the drawing loop
